17a41907e7eda6a8171811aa3705c1500c33fef3
nora-parpora <noraparpora@gmail.com>
2022-04-27 16:38:18 +0300

    fixed staticfiles and added whitenoise

--- Buyabook/settings.py
+++ Buyabook/settings.py
@@ -140,11 +140,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 )
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
+
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+# )
+# #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 # Default primary key field type
