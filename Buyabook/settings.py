4b8f2f3bc1de90abe7cba29c794acca2657a8796
nora-parpora <noraparpora@gmail.com>
2022-04-27 21:59:46 +0300

    Fixed MEDIA_ROOT

--- Buyabook/settings.py
+++ Buyabook/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = (
 )
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
